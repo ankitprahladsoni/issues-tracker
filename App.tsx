460ba85578a617d7292837047a6744b95d5aeb93
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-01-01 18:52:57 -0500

    fix file name for sonar exclusion

--- App.tsx
+++ App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
-import { Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <AppNavigator />
-    );
+    return <AppNavigator />;
   }
 }
