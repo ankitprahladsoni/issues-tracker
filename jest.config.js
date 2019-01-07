34e2ffe114ff808c2c578d163a00ea0a4591dfd4
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-01-07 01:42:35 -0500

    create modal mapper for jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-expo',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        jsx: 'react',
+      },
+    },
+  },
+  setupTestFrameworkScriptFile: '<rootDir>__tests__/SetupTests.js',
+  cacheDirectory: '.jest/cache',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+};
