1c095e668ce131f6a200fbba380bd6ad638ddfda
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-01-11 21:04:00 -0500

    upgrade react-navigation to V3

--- screens/RepositoryScreen.tsx
+++ screens/RepositoryScreen.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { createMaterialTopTabNavigator } from 'react-navigation';
+import {
+  createMaterialTopTabNavigator,
+  createAppContainer,
+} from 'react-navigation';
 import BugsScreen from './BugsScreen';
 import MilestoneScreen from './MilestoneScreen';
 import StoriesScreen from './StoriesScreen';
 
-const TabNavigator = createMaterialTopTabNavigator({
-  Milestone: MilestoneScreen,
-  Stories: StoriesScreen,
-  Bugs: BugsScreen,
-});
+const TabNavigator = createAppContainer(
+  createMaterialTopTabNavigator({
+    Milestone: MilestoneScreen,
+    Stories: StoriesScreen,
+    Bugs: BugsScreen,
+  })
+);
 
 export default class RepositoryScreen extends React.Component {
   static navigationOptions = {
