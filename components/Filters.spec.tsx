2a57f5cab34d4169e5506f637d970d7dc7ec29c9
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-01-17 23:33:32 -0500

    show filter modal on press

--- components/Filters.spec.tsx
+++ components/Filters.spec.tsx
@@ -1,9 +1,26 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { elementFromWrapper } from 'testUtils/ElementUtils';
+import {
+  elementFromWrapper,
+  propsForElementInsideWrapper,
+} from 'testUtils/ElementUtils';
 import Filters from './Filters';
+import { Modal } from 'react-native';
 
 const wrapper = shallow(<Filters type="milestone" />);
 it('should have Button called Filters', () => {
-  expect(elementFromWrapper(wrapper, 'filters').length).toBe(1);
+  expect(elementFromWrapper(wrapper, 'filters-button').length).toBe(1);
+});
+
+describe('When pressed', () => {
+  it('should open filter modal', () => {
+    propsForElementInsideWrapper(wrapper, 'filters-button').onPress();
+    expect(
+      wrapper
+        .find(Modal)
+        .first()
+        .shallow()
+        .props().visible
+    ).toBeTruthy();
+  });
 });
--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Modal, Text, TouchableHighlight } from 'react-native';
+
 export interface IFiltersProps {
   type: string;
 }
 
 export default class Filters extends React.Component<IFiltersProps> {
+  state = {
+    modalVisible: false,
+  };
+
+  setModalVisible(visible: boolean) {
+    this.setState({ modalVisible: visible });
+  }
+
   render() {
     return (
       <View>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+        >
+          <View>
+            <Text>filters</Text>
+            <TouchableHighlight
+              onPress={() => this.setModalVisible(!this.state.modalVisible)}
+            >
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
         <Button
-          testID="filters"
+          testID="filters-button"
           title="Filters"
-          onPress={() => console.log('Filters called')}
+          onPress={() => {
+            this.setModalVisible(true);
+          }}
         />
       </View>
     );
