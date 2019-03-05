3912280175cf93ffec3aed5f1fbf2b59853c65d2
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-03-04 21:53:40 -0500

    change to FC

--- src/project/issues/checkListCounter/TaskListCounter.tsx
+++ src/project/issues/checkListCounter/TaskListCounter.tsx
@@ -3,30 +3,26 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Octicons } from '@expo/vector-icons';
 import Task from '../Task';
 
-export interface TaskListCounterProps {
+interface TaskListCounterProps {
   tasks: Task[];
 }
 
-export default class TaskListCounter extends React.Component<
-  TaskListCounterProps
-> {
-  render() {
-    const tasks: Task[] = this.props.tasks;
+export default ({ tasks }: TaskListCounterProps) => {
     if (tasks.length === 0) {
-      return <View />;
+    return null;
     }
 
     const checked = tasks.filter(t => t.checked).length;
     const counterText = `${checked}/${tasks.length} tasks completed`;
     return (
       <View style={styles.view}>
-        <Octicons name="tasklist" style={{ paddingRight: 5 }} />
+      <Octicons name="tasklist" style={styles.icon} />
         <Text testID="counterText">{counterText}</Text>
       </View>
     );
-  }
-}
+};
 
 const styles = StyleSheet.create({
   view: { flexDirection: 'row', alignItems: 'center' },
+  icon: { paddingRight: 5 },
 });
