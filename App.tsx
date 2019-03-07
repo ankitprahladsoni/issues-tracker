cd18e110b02effeb686033c075d6443deda291dc
ankitprahladsoni <ankitprahladsoni@gmail.com>
2019-03-06 23:59:19 -0500

    Revert "fix font loading"
    This reverts commit 0f342eb252e99ff108ae5e8945664145141ddc84.
    revert only font loading

--- App.tsx
+++ App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import AppNavigator from './src/navigation/AppNavigator';
-import { Font } from 'expo';
-
 const Container = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
-  async componentWillMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-    });
-  }
   render() {
     return <Container />;
   }
